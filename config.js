ca2603d819881f64e2dac9aeb1e1dcf9d6e48655
Roben183 <113070641+Roben183@users.noreply.github.com>
2023-06-11 02:54:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+964 781 566 5490', 'KAMI', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
